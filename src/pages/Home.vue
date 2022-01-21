cc69687a5fb3f8048d0a28b7c9f8d17c24ab842a
Luiz André Tavares <luizandre.tavares@outlook.com>
2022-01-21 08:15:43 -0300

    inscrições

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,15 +10,15 @@
       navigation
       navigation-position="bottom"
       padding
-      class="transparent"
+      class="transparent q-px-xl"
     >
-      <q-carousel-slide name="tv" class="row no-wrap items-center">
+      <q-carousel-slide name="tv" class="row no-wrap items-center q-py-xl">
         <div id="aovivo" class="vertical-text col-1 q-mx-sm text-white">
           <p>AO VIVO</p>
         </div>
 
-        <div id="video" class="col-11">
-          <q-video :ratio=16/9 src="https://www.youtube.com/embed/_chmTnA--9I"/>
+        <div class="col">
+          <q-video id="video" :ratio=16/9 src="https://www.youtube.com/embed/_chmTnA--9I"/>
         </div>
       </q-carousel-slide>
       <q-carousel-slide
@@ -61,7 +61,7 @@
         propostas que surgem e têm potencial para serem replicadas ou ativar
         outras ações, pesquisas e iniciativas
       </p>
-      <p class="bluegreat">É O "MATCH DA INOVAÇÃO"</p>
+      <p class="bluegreat text-h4">É O "MATCH DA INOVAÇÃO"</p>
       <p>
         porque aproxima projetos e experiências da cidade com quem pode ajudar a
         concretizá-lo ou melhorá-los.
@@ -78,6 +78,88 @@
       </p>
     </div>
   </q-page>
+  <q-page class='projeto2 q-pa-xl row'>
+        <div
+      class="col column justify-center  text-white text-right "
+    >
+      <p class='col-auto text-h5 q-pa-sm'>
+        Os Laboratórios de Inovação Cidadã, os Labics, são baseados numa metodologia que valoriza a formação de equipes de trabalho multidisciplinares interessadas em participas e colaborar com a produção e difusão de protótipos, produtos e serviços que apontem soluções para questões desafiadoras e concretas das cidades e/ou de seus teritórios
+      </p>
+      <p class="bluegreat col-auto text-h4 q-pa-sm">Os Labics sistematizam, desenvolvem, apoiam e aceleram propostas com potencial para serem replicadas ou que possam ativar outras ações pesquisas e iniciativas.</p>
+
+    </div>
+    <div class="q-pa-xl col-6 text-left column justify-center">
+      <div class="projeto-header col-auto">
+        <p class="align-center no-padding no-margin text-no-wrap">O PROJETO</p>
+        <q-separator size="5px" color="purple-13"></q-separator>
+      </div>
+
+      <p class="projeto-subheader col-auto q-mt-xl text-no-wrap">
+        INOVAÇÃO CIDADÃ
+      </p>
+    </div>
+
+  </q-page>
+
+  <q-page class='row eixos q-pa-xl'>
+    <div class="q-pa-xl col-3 text-left column justify-center text-right">
+      <div class="projeto-header2 col-auto">
+        <p class="align-center no-padding no-margin text-no-wrap">O PROJETO</p>
+        <q-separator size="5px" color="primary"></q-separator>
+      </div>
+
+      <p class="projeto-subheader col-auto q-mt-xl text-no-wrap">
+        EIXOS
+      </p>
+    </div>
+    <div class="col-9  ">
+
+      <div class="row">
+      <div class="col-4 q-pa-xl no-wrap" v-for="eixo in eixos" :key='eixo'>
+        <div class="column" >
+            <q-icon color="primary" size="50px" class='col' :name='eixo.icon'/>
+            <p class='col text-white text-h5 no-margin text-uppercase'>{{eixo.title}}</p>
+            <p class="col text-body1 text-weight-medium" >{{eixo.description}}</p>
+        </div>
+      </div>
+      </div>
+
+    </div>
+  </q-page>
+
+  <q-page class="row mundo q-pa-xl">
+    <div id="aovivo" class="vertical-text col-1 q-mx-sm">
+          <p class='color-secondary'>O PROJETO</p>
+        </div>
+
+        <div class="col q-ma-md flex">
+          <p class="text-h2 color-primary">LABORATÓRIOS NO MUNDO</p>
+          <p class="fill-text text-body text-white text-justify">O Laboratório de Inovação Cidadã é um projeto Secretaria-Geral Ibero-americana que tem como objetivo promover a inovação cidadã a fim de fomentar a transformação social, a governança democrática, e o desenvovimento social, cultural e econômico. Inovação Cidadã é um processo que resolve problemas sociais com tecnologias(digitais, sociais, ancestrais) e metodologias inovadoras, através do envolvimento da própria comunidade afetada. Com isso, os cidadãos deixam de ser receptores passivos de ações institucionais e se convertem em protagonistas e produtores de suas próprias soluções.
+          </p>
+        </div>
+        <div class="col flex items-center justify-center">
+          <q-icon class='pink' size="40vw" name='language'>
+          </q-icon>
+          <q-btn  outline size='1.5vw' color='primary' label="Saiba Mais Aqui">
+
+          </q-btn>
+        </div>
+  </q-page>
+
+  <q-page class="hero row q-pa-xl">
+        <div id="aovivo" class="vertical-text col-1 q-mx-sm">
+          <p class='color-primary text-uppercase'>Inscrições</p>
+        </div>
+
+        <div class="col column q-ma-md justify-center items-center">
+          <p class="col-auto text-h2 text-white text-center">Estão abertas até o dia 30 de setembro</p>
+          <p class="col-auto text-h4 text-white text-center">as inscrições para o Monitora Labic. Nesta edição, vamos selecionar 13 projetos que já participaram de alguma edição anterior. Serão oferecidos mentorias específicas para recomeçar e fortalecer suas atividades.
+          </p>
+          <q-btn id='rounder' size='xl' style="width: 40%;" class='q-ma-xl text-white' label='ACESSE A CHAMADA'></q-btn>
+        </div>
+
+  </q-page>
+  
 </template>
 
 <script>
@@ -86,6 +168,42 @@ import { ref } from "vue";
 
 export default defineComponent({
   name: "PageIndex",
+  data: function(){
+    return{
+      eixos:[
+        {
+          icon:'smartphone',
+          title:'Mídias, Ações Virtuais e Diversidade',
+          description:'Projetos, ações, aplicativos e tecnologias que apresentem iniciativas de comunicação em qualquer suporte e linguagem e que estimulem a diuversidade de conteúdos e de produtos de mídias.',
+        },
+        {
+          icon:'pie_chart',
+          title:'Tecnologias, Redes, Dados e Plataformas',
+          description:'Iniciativas de desenvolvimento de plataformas, redes, aplicativos, programas, tecnologias abertas. Projetos de mineração, análise e visualização de dados.',
+        },
+        {
+          icon:'auto_stories',
+          title:'Formação',
+          description:'Projetos, propostas, metodologias de formação e educação utilizando qualquer linguagem; formação em rede, formação livre, educação inclusiva, formação de agentes e multiplicadores, formação para a diversidade; educação e inovação.',
+        },
+        {
+          icon:'business',
+          title:'Economia e Cidadania',
+          description:'Iniciativas em novas economias, sustentabilidade, financiamento colaborativo, empreendedorismo social, redes de proteção, inclusão, visibilidade, ampliação de direitos para a diversidade cultural, de gênero, religiosa, étnica, etc.',
+        },
+        {
+          icon:'coronavirus',
+          title:'Ações de enfrentamento a covid-19',
+          description:'no campo da saúde, comunicação, da cultura, educacionais e em todos os campos do conhecimento, constituição de redes de apoio e solidariedade pós-pandemia.',
+        },
+        {
+          icon:'add',
+          title:'Outros temas',
+          description:'',
+        },
+      ]
+    }
+  },
 
   setup() {
     return {
@@ -109,8 +227,9 @@ export default defineComponent({
 }
 
 #aovivo {
-  min-height: 49.8vw;
-  border-left: 5px solid white;
+  height: 49.8vw;
+  max-height: 80.8vh;
+  border-left: 3px solid white;
 }
 
 #aovivo p {
@@ -129,6 +248,11 @@ export default defineComponent({
   font-size: 3em;
 }
 
+.projeto-header2 p {
+  color: #00d3de;
+  font-size: 3em;
+}
+
 .projeto-left {
   background-color: #8b50ff;
 }
@@ -143,9 +267,44 @@ export default defineComponent({
 }
 
 .bluegreat {
-  font-size: 2.5rem;
   text-shadow: 1px 1px black;
   color: #00d3de;
   font-weight: bold;
 }
+
+
+.projeto2{
+background-color:#8b50ff}
+
+.mundo{
+background-color:#8b50ff}
+
+.eixos{
+  background-color: #df71ff
+}
+
+.color-primary{
+  color:#00d3de;
+}
+
+.color-secondary{
+  color:#df71ff;
+}
+
+.pink{
+  color:#df71ff
+}
+
+.fill-text{
+  font-size: 1.5vw;
+}
+
+#rounder{
+border-radius: 75px 75px;
+            padding: 35px;
+            text-align: center;
+            background-color: #df71ff;
+
+}
+
 </style>
